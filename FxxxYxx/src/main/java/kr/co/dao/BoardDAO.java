3e3e8bbae7d4ee7efb1e650f6ea752d1521f37e1
LeoDZ <thdfpdh@naver.com>
2023-12-13 16:38:58 +0900

    test2

---
+++ FxxxYxx/src/main/java/kr/co/dao/BoardDAO.java
@@ -0,0 +1,18 @@
+package kr.co.dao;
+
+import java.util.List;
+
+import kr.co.vo.BoardVO;
+
+public interface BoardDAO {
+
+	// 게시글 작성
+	public void write(BoardVO boardVO) throws Exception;
+	
+	// 게시물 목록 조회
+	public List<BoardVO> list() throws Exception;
+	
+	// 게시물 조회
+	public BoardVO read(int bno) throws Exception;
+	
+}
